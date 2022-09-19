a9422ef927d9033e1bd70983bc9bbfc0a49f1e3d
Christopher Murphy <flyweight@pm.me>
2022-09-19 10:26:10 -0600

    feat: add usage flag

--- cmd/fname/fname.go
+++ cmd/fname/fname.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"os"
 
 	"github.com/spf13/pflag"
 	"github.com/splode/fname"
 )
 
+const (
+	usage = `fname generates random, human-friendly names, such as "awful-fossil"
+or "zesty zebra".
+
+Usage: 
+  fname [options]
+
+Examples:
+  # generate a single name phrase using default options
+  fname
+
+  # generate 10 names using a custom delimiter
+  fname --delimiter "." --number 10
+
+Options:`
+
+	contact = `
+Author: Christopher Murphy <flyweight@pm.me>
+Repo: https://github.com/splode/fname`
+)
+
+//go:embed banner
+var banner []byte
+
 func main() {
+	pflag.Usage = func() {
+		fmt.Println(string(banner))
+		fmt.Println(usage)
+		pflag.PrintDefaults()
+		fmt.Println(contact)
+	}
+
 	var (
 		delimiter string = ""
+		help      bool   = false
 		number    int    = 1
 		seed      int64  = -1
 	)
 
 	pflag.StringVarP(&delimiter, "delimiter", "d", delimiter, "delimiter to use between words")
+	pflag.BoolVarP(&help, "help", "h", help, "Show fname usage")
 	pflag.IntVarP(&number, "number", "n", number, "number of names to generate")
 	pflag.Int64VarP(&seed, "seed", "s", seed, "random generator seed")
 	pflag.Parse()
+
+	if help {
+		pflag.Usage()
+		os.Exit(0)
+	}
 
 	opts := []fname.RandomNameGeneratorOption{}
 	if delimiter != "" {
